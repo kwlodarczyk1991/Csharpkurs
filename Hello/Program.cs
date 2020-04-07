19c322150aa509b55f3d983b81b88d27a229bb59
Krzysiek <Krzysiek@DESKTOP-QPN7HML>
2020-04-07 21:27:26 +0200

    pierwszy program

---
+++ Hello/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string names = args[0];
+            //Console.WriteLine(" Siema, " + names); 
+            foreach (var item in args)
+            {
+                Console.WriteLine("Siema, " + item);
+            }
+        }
+    }
+}
