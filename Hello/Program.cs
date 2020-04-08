e67339c827327c510f5debac307e362c3c2a658b
kwlodarczyk1991 <krzoku91@gmail.com>
2020-04-08 21:08:53 +0200

    "First program done"

--- Hello/Program.cs
+++ Hello/Program.cs
@@ -5,14 +5,70 @@ namespace Hello
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
-            //string names = args[0];
-            //Console.WriteLine(" Siema, " + names); 
-            foreach (var item in args)
+            valueTypes();
+
+            for (; ; )
             {
-                Console.WriteLine("Siema, " + item);
+                Greeting();
+                CheckAge();
+                Settings();
             }
+
+
+
+        }
+
+        private static void valueTypes()
+        {
+            int maxInt = int.MaxValue;
+            int minInt = int.MinValue;
+            long maxLong = long.MaxValue;
+            long minLong = long.MinValue;
+
+            Console.WriteLine("maxInt = " + maxInt);
+            Console.WriteLine("minInt = " + minInt);
+            Console.WriteLine("maxLong = " + maxLong);
+            Console.WriteLine("minLong = " + minLong);
+                
+        }
+
+        private static void Settings()
+        {
+            Console.ReadKey();
+            Console.Clear();
+            Console.ResetColor();
+        }
+
+        private static void CheckAge()
+        {
+            Console.Write("What is your age?");
+            int age;
+            bool result = int.TryParse(Console.ReadLine(), out age);
+
+            if (age > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.WriteLine("Great lets drink beer");
+            }
+
+            else if (result == false)
+            {
+                Console.WriteLine("Give only numbers!");
+            }
+
+            else
+            {
+                Console.WriteLine("Ooops maybe milk?");
+            }
+        }
+
+        private static void Greeting()
+        {
+            Console.WriteLine("Give me your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name);
         }
     }
 }
